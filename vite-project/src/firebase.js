eda3208d59bd50f9de3d6efd66a07f7dafffd293
suleman15 <sulemanahmed2704@gmail.com>
2023-12-26 18:51:05 +0500

    lastCmt

--- vite-project/src/firebase.js
+++ vite-project/src/firebase.js
@@ -1,8 +1,8 @@
- // Import the functions you need from the SDKs you need
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+
 import { initializeApp } from "firebase/app";
-import {getAuth} from 'firebase/auth'
+import {getAuth, GoogleAuthProvider} from 'firebase/auth'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -14,6 +14,7 @@ const firebaseConfig = {
     appId: "1:816759664307:web:b555489ade8efff5c2cac6"
   };
   
-  // Initialize Firebase
-  export const app = initializeApp(firebaseConfig);
-export   const auth = getAuth(app);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth =  getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
